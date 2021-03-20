bc12312a24ae332f0426e7d93fbb44511ccd4c3d
Pablo Buisson <pablo.buisson@gmail.com>
2021-03-20 10:06:31 +0100

    add example

--- lib/screens/sliver_to_box_adapter.dart
+++ lib/screens/sliver_to_box_adapter.dart
@@ -9,6 +9,58 @@ class CustomSliverToBoxAdapter extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('SliverToBoxAdapter'),
+        centerTitle: true,
+      ),
+      body: CustomScrollView(
+        slivers: [
+          // SliverToBoxAdapter converts boxes to slivers
+          // Allows RenderBox objects in CustomScrollView
+          SliverToBoxAdapter(
+            child: Container(
+              width: double.infinity,
+              height: 200.0,
+              color: Colors.deepPurple,
+              child: Center(
+                child: Text(
+                  'Header',
+                  textScaleFactor: 1.3,
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+          ),
+          SliverFixedExtentList(
+            itemExtent: 50,
+            delegate: SliverChildBuilderDelegate(
+                  (context, index) {
+                return Container(
+                  alignment: Alignment.center,
+                  color: Colors.deepPurple[100 * (index % 9)],
+                  child: Text('Deep purple $index'),
+                );
+              },
+              childCount: 9,
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Container(
+              width: double.infinity,
+              height: 200.0,
+              color: Colors.deepPurple[900],
+              child: Center(
+                child: Text(
+                  'Footer',
+                  textScaleFactor: 1.3,
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
